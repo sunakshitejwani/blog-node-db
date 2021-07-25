fad326b53e9d9ee2b55378a39abaf31f8ec3fe4c
sunakshitejwani <sunakshitejwani@gmail.com>
2021-07-25 19:01:10 +0530

    save composed posts with mongodb

--- app.js
+++ app.js
@@ -3,60 +3,88 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require('lodash');
+const mongoose = require("mongoose");
+const _ = require("lodash");
 let posts = [];
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent =
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent =
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent =
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/",function(req,res){
-  res.render("home",{content:homeStartingContent, homePosts:posts});
-})
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
-app.get("/about",function(req,res){
-  res.render("about",{content:aboutContent});
-})
+const postSchema = {
+  title: String,
+  content: String
+};
 
-app.get("/contact",function(req,res){
-  res.render("contact",{content:contactContent});
-})
+const Post = mongoose.model("Post", postSchema);
 
-app.get("/compose",function(req,res){
+app.get("/", function(req, res) {
+  res.render("home", { content: homeStartingContent, homePosts: posts });
+});
+
+app.get("/about", function(req, res) {
+  res.render("about", { content: aboutContent });
+});
+
+app.get("/contact", function(req, res) {
+  res.render("contact", { content: contactContent });
+});
+
+app.get("/compose", function(req, res) {
   res.render("compose");
-})
+});
 
-app.get("/posts/:pageTitle",function(req,res){
+app.get("/posts/:pageTitle", function(req, res) {
   console.log(req.params.pageTitle);
   let requestedTitle = _.kebabCase(req.params.pageTitle);
-  posts.forEach(function(element){
+  posts.forEach(function(element) {
     let storedTitle = _.kebabCase(element.title);
-    if(requestedTitle === storedTitle){
+    if (requestedTitle === storedTitle) {
       console.log("Match found");
-      res.render("post",{content:element.content, title: element.title, titleSearched : requestedTitle});
+      res.render("post", {
+        content: element.content,
+        title: element.title,
+        titleSearched: requestedTitle
+      });
     } else {
-      console.log("Not a match")
+      console.log("Not a match");
     }
-  })
-})
+  });
+});
 
-app.post("/compose",function(req,res){
+app.post("/compose", function(req, res) {
   const post = {
-    title:req.body.postTitle,
-    content:req.body.postBody
-  }
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
   posts.push(post);
+  const newPost = new Post ({
+
+    title: req.body.postTitle,
+ 
+    content: req.body.postBody
+ 
+  });
+  console.log(newPost);
+  newPost.save();
   res.redirect("/");
-})
+});
 
-
-app.listen(3000, function(req,res){
+app.listen(3000, function(req, res) {
   console.log("server is running on port 3000");
 });
