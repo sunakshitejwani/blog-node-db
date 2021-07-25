b6082fad19d0fd265740750bc8b96b2c1d42b8be
sunakshitejwani <sunakshitejwani@gmail.com>
2021-07-25 19:04:15 +0530

    get the home page to render the posts

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-let posts = [];
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -34,7 +33,18 @@ const postSchema = {
 const Post = mongoose.model("Post", postSchema);
 
 app.get("/", function(req, res) {
-  res.render("home", { content: homeStartingContent, homePosts: posts });
+  //res.render("home", { content: homeStartingContent, homePosts: posts });
+  Post.find({}, function(err, posts){
+
+    res.render("home", {
+  
+      content: homeStartingContent,
+  
+      homePosts: posts
+  
+      });
+  
+  });
 });
 
 app.get("/about", function(req, res) {
